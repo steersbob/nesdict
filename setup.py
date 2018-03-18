298baccaa92c042fca18ef8b49d684bdb5b2a62d
Bob Steers <steersbob@gmail.com>
2018-03-18 21:54:11 +0100

    use pip env

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='nesdict',
-    version_format='{tag}.dev{commitcount}+{gitsha}',
-    setup_requires=['setuptools-git-version'],
+    use_scm_version=True,
     description='Support for xpath-like lookups in nested Python dicts.',
     long_description=open('README.md').read(),
-    url='https://github.com/Kargathia/nesdict',
-    download_url='https://github.com/Kargathia/nesdict/archive/0.1.tar.gz',
+    url='https://github.com/steersbob/nesdict',
+    download_url='https://github.com/steersbob/nesdict/archive/0.1.tar.gz',
     author='Bob Steers',
-    author_email='kargathia@hotmail.com',
+    author_email='steers.bob@gmail.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
@@ -23,5 +22,6 @@ setup(
     keywords='xpath dicts nested lookup',
     packages=find_packages(exclude=['tests']),
     install_requires=['dpath'],
-    extras_require={'dev': ['tox']}
+    extras_require={'dev': ['tox', 'pipenv']},
+    setup_requires=['setuptools_scm'],
 )
