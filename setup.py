9836ad69c3d774ced2a1a7f1160bb08737462892
Bob Steers <kargathia@hotmail.com>
2018-05-15 12:21:04 +0200

    Drop support for 3.4

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
